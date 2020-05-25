43e754cd4bcffefcf4300836cb7ff7ca9118feee
Maxim <financial_monster@mail.ru>
2020-05-25 16:25:17 +0400

    Add the FilmButtons component

---
+++ src/components/filmButtons/FilmButtons.tsx
@@ -0,0 +1,26 @@
+import React, { FC, memo } from 'react';
+
+type TFilmButtonsProps = {
+    playBtnClickHandler: () => void;
+}
+
+export const FilmButtons: FC<TFilmButtonsProps> = memo(({playBtnClickHandler}) => {
+    return (
+        <>
+            <button className="btn btn--play movie-card__button" type="button" title="Play"
+                onClick={playBtnClickHandler}
+            >
+                <svg viewBox="0 0 19 19" width="19" height="19">
+                    <use xlinkHref="#play"></use>
+                </svg>
+                <span>Play</span>
+            </button>
+            <button className="btn btn--list movie-card__button" type="button" title="My list">
+                <svg viewBox="0 0 19 20" width="19" height="20">
+                    <use xlinkHref="#add"></use>
+                </svg>
+                <span>My list</span>
+            </button>
+        </>
+    );
+});
