9cd659295bef1bd6858ea2429b4405e07205488a
Maxim <financial_monster@mail.ru>
2020-05-28 14:53:56 +0400

    Add the test of the LoginForm component

--- src/components/loginForm/LoginForm.test.js
+++ src/components/loginForm/LoginForm.test.js
@@ -1,52 +1,49 @@
 import React from 'react';
 import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { act } from '@testing-library/react';
 import eventually from "wix-eventually";
 import configureStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
-import { storeMock } from 'mocks';
+import { createStoreMock } from 'mocks';
 
 import { LoginForm } from './LoginForm';
-import { authActions } from 'domains/auth/authActions';
 
 Enzyme.configure({adapter: new Adapter()});
 
 describe(`LoginForm:`, () => {
     it(`default action of the loginForm after submission should be prevented`, async () => {
         const preventDefault = jest.fn();
+        const mockStore = configureStore();
+        const store = mockStore(createStoreMock());
 
         const loginForm = mount(
-            <LoginForm loginFormSubmitHandler={() => {}} />
+            <Provider store={store}>
+                <LoginForm loginFormSubmitHandler={() => {}} />
+            </Provider>
         );
 
-        act(() => {
-            const form = loginForm.find(`.sign-in__form`).at(0);
-            const emailInput = loginForm.find(`#user-email`).at(0);
-            const passwordInput = loginForm.find(`#user-password`).at(0);
+        const form = loginForm.find(`.sign-in__form`).at(0);
 
-            emailInput.simulate(`change`, { target: {value: `example@mail.ru`, name: `email`} });
-            passwordInput.simulate(`change`, { target: {value: `exampletest`, name: `password`} });
-
-            form.simulate(`submit`, {
-                preventDefault
-            });
+        form.simulate(`submit`, {
+            preventDefault
         });
 
-        // await eventually(() => {
-            // loginForm.update();
+        await eventually(() => {
+            loginForm.update();
 
             expect(preventDefault).toHaveBeenCalledTimes(1);
             expect(preventDefault.mock.calls[0][0]).toEqual(void 0);
-        // });
+        });
     });
 
-    it.only(`should dispatch fetchAuthRequest action after submission of the loginForm with right values of the email
-        and password inputs`,
+    it(`loginFormSubmitHandler callback should be invoked with right values on the submission of the
+        loginForm`,
     async () => {
-        const mockStore = configureStore();
-        const store = mockStore(storeMock);
+        jest.setTimeout(15000);        
+        
         const loginFormSubmitHandler = jest.fn();
+        const mockStore = configureStore();
+        const store = mockStore(createStoreMock());
 
         const loginForm = mount(
             <Provider store={store}>
@@ -54,27 +51,76 @@ describe(`LoginForm:`, () => {
             </Provider>
         );
 
-        jest.setTimeout(30000);        
+        const form = loginForm.find(`.sign-in__form`).at(0);
+        const emailInput = loginForm.find(`#user-email`).at(0);
+        const passwordInput = loginForm.find(`#user-password`).at(0);
 
-        act(() => {
-            const form = loginForm.find(`.sign-in__form`).at(0);
-            const emailInput = loginForm.find(`#user-email`).at(0);
-            const passwordInput = loginForm.find(`#user-password`).at(0);
+        emailInput.simulate(`change`, { target: {value: `example@mail.ru`, name: `email`}});
+        passwordInput.simulate(`change`, { target: {value: `exampletest`, name: `password`}});
+        form.simulate(`submit`);
 
-            emailInput.simulate(`change`, { target: {value: 'example@mail.ru', name: `email`} });
-            passwordInput.simulate(`change`, { target: {value: 'exampletest', name: `password`} });
-
-            form.simulate(`submit`);
-        });
-
-        // await new Promise((resolve) => setTimeout(resolve, 1000));
         await eventually(() => {
-            loginForm.update();
-
             expect(loginFormSubmitHandler).toHaveBeenCalledTimes(1);
-            expect(loginFormSubmitHandler.mock.calls[0][0]).toEqual(void 0);
-            // expect(store.getActions().length).toBe(1);
-            // expect(store.getActions()[0]).toEqual(authActions.fetchAuthRequest({email: `example@mail.ru`, password: `exampletest`}));
+            expect(loginFormSubmitHandler.mock.calls[0][0]).toEqual({
+                email: `example@mail.ru`,
+                password: `exampletest`
+            });
+        });
+    });
+
+    it(`loginFormSubmitHandler callback should not be invoked on the submission of the
+        loginForm if the value of email field is not valid`,
+    async () => {
+        jest.setTimeout(15000);        
+        
+        const loginFormSubmitHandler = jest.fn();
+        const mockStore = configureStore();
+        const store = mockStore(createStoreMock());
+
+        const loginForm = mount(
+            <Provider store={store}>
+                <LoginForm loginFormSubmitHandler={loginFormSubmitHandler} />
+            </Provider>
+        );
+
+        const form = loginForm.find(`.sign-in__form`).at(0);
+        const emailInput = loginForm.find(`#user-email`).at(0);
+        const passwordInput = loginForm.find(`#user-password`).at(0);
+
+        emailInput.simulate(`change`, { target: {value: `example`, name: `email`}});
+        passwordInput.simulate(`change`, { target: {value: `exampletest`, name: `password`}});
+        form.simulate(`submit`);
+
+        await eventually(() => {
+            expect(loginFormSubmitHandler).toHaveBeenCalledTimes(0);
+        });
+    });
+
+    it(`loginFormSubmitHandler callback should not be invoked on the submission of the
+        loginForm if the value of password field is not valid`,
+    async () => {
+        jest.setTimeout(15000);        
+        
+        const loginFormSubmitHandler = jest.fn();
+        const mockStore = configureStore();
+        const store = mockStore(createStoreMock());
+
+        const loginForm = mount(
+            <Provider store={store}>
+                <LoginForm loginFormSubmitHandler={loginFormSubmitHandler} />
+            </Provider>
+        );
+
+        const form = loginForm.find(`.sign-in__form`).at(0);
+        const emailInput = loginForm.find(`#user-email`).at(0);
+        const passwordInput = loginForm.find(`#user-password`).at(0);
+
+        emailInput.simulate(`change`, { target: {value: `example@mail.ru`, name: `email`}});
+        passwordInput.simulate(`change`, { target: {value: `test`, name: `password`}});
+        form.simulate(`submit`);
+
+        await eventually(() => {
+            expect(loginFormSubmitHandler).toHaveBeenCalledTimes(0);
         });
     });
 });
--- src/components/loginForm/LoginForm.tsx
+++ src/components/loginForm/LoginForm.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, memo } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { useSelector } from 'react-redux';
 
@@ -7,60 +7,62 @@ import { loginFormShape } from 'formShapes';
 import { TUserData } from 'types';
 
 type TLoginFormProps = {
-    loginFormSubmitHandler: (userData: TUserData) => void
+    loginFormSubmitHandler: (userData: TUserData) => void;
 }
 
-export const LoginForm: FC<TLoginFormProps> = React.memo(({loginFormSubmitHandler}) => {
+export const LoginForm: FC<TLoginFormProps> = memo(({loginFormSubmitHandler}) => {
     const authError = useSelector(authSelectors.getAuthError);
 
     return (
         <Formik initialValues={loginFormShape.initialValues} onSubmit={loginFormSubmitHandler}
             validationSchema={loginFormShape.schema}
         >
-            <Form className="sign-in__form">
-                <ErrorMessage name="email">
-                    {(message) => (
+            {({ isValid }) => (
+                <Form className="sign-in__form">
+                    <ErrorMessage name="email">
+                        {(message) => (
+                            <div className="sign-in__message">
+                                <p>{message}</p>
+                            </div>
+                        )}                   
+                    </ErrorMessage>
+                    <ErrorMessage name="password">
+                        {(message) => (
+                            <div className="sign-in__message">
+                                <p>{message}</p>
+                            </div>
+                        )}
+                    </ErrorMessage>
+                    {(authError) &&
                         <div className="sign-in__message">
-                            <p>{message}</p>
+                            <p>We can’t recognize this email <br/> and password combination. Please try again.</p>
                         </div>
-                    )}                   
-                </ErrorMessage>
-                <ErrorMessage name="password">
-                    {(message) => (
-                        <div className="sign-in__message">
-                            <p>{message}</p>
+                    }
+                    <div className="sign-in__fields">
+                        <div className="sign-in__field">
+                            <Field className="sign-in__input" type="email" placeholder="Email address" name="email"
+                                id="user-email"
+                            />
+                            <label className="sign-in__label visually-hidden" htmlFor="user-email">
+                                Email address
+                            </label>
                         </div>
-                    )}
-                </ErrorMessage>
-                {(authError) &&
-                    <div className="sign-in__message">
-                        <p>We can’t recognize this email <br/> and password combination. Please try again.</p>
+                        <div className="sign-in__field">
+                            <Field className="sign-in__input" type="password" placeholder="Password" name="password"
+                                id="user-password"
+                            />
+                            <label className="sign-in__label visually-hidden" htmlFor="user-password">
+                                Password
+                            </label>
+                        </div>
                     </div>
-                }
-                <div className="sign-in__fields">
-                    <div className="sign-in__field">
-                        <Field className="sign-in__input" type="email" placeholder="Email address" name="email"
-                            id="user-email"
-                        />
-                        <label className="sign-in__label visually-hidden" htmlFor="user-email">
-                            Email address
-                        </label>
+                    <div className="sign-in__submit">
+                        <button className="sign-in__btn" type="submit" title="Sign in" disabled={!isValid}>
+                            Sign in
+                        </button>
                     </div>
-                    <div className="sign-in__field">
-                        <Field className="sign-in__input" type="password" placeholder="Password" name="password"
-                            id="user-password"
-                        />
-                        <label className="sign-in__label visually-hidden" htmlFor="user-password">
-                            Password
-                        </label>
-                    </div>
-                </div>
-                <div className="sign-in__submit">
-                    <button className="sign-in__btn" type="submit" title="Sign in">
-                        Sign in
-                    </button>
-                </div>
-            </Form>
+                </Form>
+            )}
         </Formik>
     );
 });
