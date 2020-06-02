c91c70aa6c4b668a9bab6350272357c4f7fefd3b
Maxim <financial_monster@mail.ru>
2020-06-02 10:19:36 +0400

    Updates

---
+++ precache-manifest.e3c8672f1a65ff86766fb77ca6c20839.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc7398177882c610c13628b3c6ad4621",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d95f42ab413b8ebe199",
+    "url": "/static/js/2.1a005a8d.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/static/js/2.1a005a8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44d7f15576a3526b0694",
+    "url": "/static/js/main.7ec6aabc.chunk.js"
+  },
+  {
+    "revision": "c8cfa6cdc17b356e6cbe",
+    "url": "/static/js/runtime-main.490adba3.js"
+  }
+]);
