d3dd788fcae4a85d8b55afdec9d86e5abbfb91e4
Maxim <financial_monster@mail.ru>
2020-06-03 12:34:59 +0400

    Updates

---
+++ precache-manifest.4fa041986ffa9f93fce67e2f63051ab2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65b1538b67ffe2f292af86fd5a781794",
+    "url": "/whattowatch/index.html"
+  },
+  {
+    "revision": "201192e29bd08fe68a32",
+    "url": "/whattowatch/static/js/2.beb87c0b.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/whattowatch/static/js/2.beb87c0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70fd1d85e1a0e54f550f",
+    "url": "/whattowatch/static/js/main.478990b4.chunk.js"
+  },
+  {
+    "revision": "c5307c9fe050fdcb02dd",
+    "url": "/whattowatch/static/js/runtime-main.eb27ced4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.416424de7ddcb2012c43dfe0722e83e9.js"
+  "/whattowatch/precache-manifest.4fa041986ffa9f93fce67e2f63051ab2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/whattowatch/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
